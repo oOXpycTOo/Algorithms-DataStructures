55766ccb7b84b1497f280c4952b3ed00e2a4fdc4
oOXpycTOo <vladkaz1999@gmail.com>
2019-03-31 19:50:19 +0300

    Finally implemented the algorithm with DSU

--- EqualitiesAndInequalities/main.cpp
+++ EqualitiesAndInequalities/main.cpp
@@ -1,7 +1,7 @@
 #include <fstream>
-#include <stack>
 #include <string>
 #include <vector>
+#include <utility>
 
 using uint8 = unsigned short;
 using int8 = short;
@@ -12,122 +12,93 @@ using int64 = long long;
 
 const int64 MIN_INT_64 = -9223372036854775807LL; 
 
-struct Variable {
-	int8 value = -1;
-	std::vector<uint32> equal_to;
-	std::vector<uint32> not_equal_to;
-	int32 priority = -1;
-};
 
-std::string is_system_solvable(std::vector<Variable>& variables, const std::vector<int32>& priorities) {
-	for(const uint32& prior_variable_idx : priorities) {
-		if(variables[prior_variable_idx].value == -1) {
-			variables[prior_variable_idx].value = 1;
+
+class DisjointSetUnion {
+	std::vector<int32> parent;
+	std::vector<int32> rank;
+	std::vector<bool> is_marked;
+public:
+	DisjointSetUnion(uint32 size) : parent(size), rank(size), is_marked(size) {}
+	void make_set(int32 value) {
+		parent[value] = value;
+		rank[value] = 0;
+		is_marked[value] = true;
+	}
+
+	int32 find(int32 value) {
+		return value == parent[value] ? value : parent[value] = find(parent[value]);
+	}
+
+	bool is_value_marked(int32 value) {
+		return is_marked[value];
+	}
+
+	void union_sets(int value_1, int value_2) {
+		if((value_1 = find(value_1)) == (value_2 = find(value_2))) {
+			return;				
 		}
-		for(const uint32& variable_idx : variables[prior_variable_idx].equal_to) {
-			if(variables[variable_idx].value == -1) {
-				variables[variable_idx].value = variables[prior_variable_idx].value;				
-			} else {
-				if(variables[prior_variable_idx].value != variables[variable_idx].value) {
-					return "No";	
-				} 
-			}
-		}
-		for(const uint32& variable_idx : variables[prior_variable_idx].not_equal_to) {
-			if(variables[variable_idx].value == -1) {
-				variables[variable_idx].value = variables[prior_variable_idx].value ^ 1;				
-			} else {
-				if(variables[prior_variable_idx].value == variables[variable_idx].value) {
-					return "No";	
-				}
+		if(rank[value_1] < rank[value_2]) {
+			parent[value_1] = value_2;
+		} else {
+			parent[value_2] = value_1;
+			if(rank[value_1] == rank[value_2]) {
+				++rank[value_1];
 			}
 		}
 	}
-	return "Yes";
-}
+};
 
 int main() {
 	std::ifstream input("equal-not-equal.in");
 	std::ofstream output("equal-not-equal.out");
-	
-	uint32 variables_amount;
-	uint32 constraints_amount;
-	input >> variables_amount >> constraints_amount;
-	std::vector<Variable> variables(variables_amount + 1);
-	std::string expression;
-	uint32 x_i;
-	uint32 x_j;
-	std::string sign;
-	input >> expression;
-	x_i = std::stoi(expression.substr(1));
-	input >> sign;
-	input >> expression;
-	x_j = std::stoi(expression.substr(1));
-	std::vector<int32> priorities;
-	priorities.push_back(x_i);
-	variables[x_i].priority = priorities.size() - 1; 
-	variables[x_i].value = 1;
-	if(x_i != x_j) {
-		priorities.push_back(x_j); 
-		variables[x_j].priority = priorities.size() - 1; 
-	}
-	if(sign == "==") {
-		variables[x_i].equal_to.push_back(x_j);
-	} else {
-		variables[x_i].not_equal_to.push_back(x_j);
-	}
-	for(uint32 i = 1; i < constraints_amount; ++i) {
-		input >> expression;
-		x_i = std::stoi(expression.substr(1));
+	uint32 variables_amount, contraints_amount;
+	input >> variables_amount>> contraints_amount;
+	int32 x_i, x_j;
+	std::string temp, sign;
+	DisjointSetUnion equal_classes(variables_amount + 1);
+	std::vector<std::pair<int32, int32>> not_equal;
+	for(uint32 i = 0; i < contraints_amount; ++i) {
+		input >> temp;
+		x_i = std::stoi(temp.substr(1));
 		input >> sign;
-		input >> expression;
-		x_j = std::stoi(expression.substr(1));
-		if(variables[x_i].priority != -1 && variables[x_j].priority != -1) {
-			if(variables[x_i].priority <= variables[x_j].priority) {
-				if(sign == "==") {
-					variables[x_i].equal_to.push_back(x_j);
-				} else {
-					variables[x_i].not_equal_to.push_back(x_j);
+		input >> temp;
+		x_j = std::stoi(temp.substr(1));
+		if(sign == "==") {
+			if(!(equal_classes.is_value_marked(x_i) || equal_classes.is_value_marked(x_j))) {
+				equal_classes.make_set(x_i);
+				if(x_i != x_j) {
+					equal_classes.make_set(x_j);
+					equal_classes.union_sets(x_i, x_j);
 				}
-			} else {
-				if(sign == "==") {
-					variables[x_j].equal_to.push_back(x_i);
-				} else {
-					variables[x_j].not_equal_to.push_back(x_i);
+
+			} else if(equal_classes.is_value_marked(x_i) && equal_classes.is_value_marked(x_j)) {
+				if(equal_classes.find(x_i) != equal_classes.find(x_j)) {
+					equal_classes.union_sets(x_i, x_j);
 				}
+			} else if(equal_classes.is_value_marked(x_i)) {
+				equal_classes.make_set(x_j);
+				equal_classes.union_sets(x_i, x_j);
+			} else if(equal_classes.is_value_marked(x_j)) {
+				equal_classes.make_set(x_i);
+				equal_classes.union_sets(x_j, x_i);
 			}
-		} else if(variables[x_i].priority == -1 && variables[x_j].priority == -1) {
-			priorities.push_back(x_i);
-			variables[x_i].priority = priorities.size() - 1;
-			priorities.push_back(x_j);
-			variables[x_j].priority = priorities.size() - 1; 
-			if(sign == "==") {
-				variables[x_i].equal_to.push_back(x_j);				
-			} else {
-				variables[x_i].not_equal_to.push_back(x_j);
+		} else {
+			if(x_i == x_j) {
+				output << "No";
+				return 0;
 			}
-		} else if(variables[x_j].priority == -1) {
-			priorities.push_back(x_j);
-			variables[x_j].priority = priorities.size() - 1; 
-			if(sign == "==") {
-				variables[x_i].equal_to.push_back(x_j);
-			} else {
-				variables[x_i].not_equal_to.push_back(x_j);
-			}
-		} else if(variables[x_i].priority == -1) {
-			priorities.push_back(x_i);
-			variables[x_i].priority = priorities.size() - 1; 
-			if(sign == "==") {
-				variables[x_j].equal_to.push_back(x_i);
-			} else {
-				variables[x_j].not_equal_to.push_back(x_i);
-			}
-		} 
+			not_equal.push_back(std::make_pair(x_i, x_j));
+		}
 	}
-
-	
-
-	output << is_system_solvable(variables, priorities); 
-
+	for(auto expression : not_equal) {
+		if(equal_classes.is_value_marked(expression.first) && equal_classes.is_value_marked(expression.second)) {
+			if(equal_classes.find(expression.first) == equal_classes.find(expression.second)) {
+				output << "No";
+				return 0;
+			}
+		}
+	}
+	output << "Yes";
 	return 0;
 }
